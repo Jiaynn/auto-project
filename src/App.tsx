5711f9795b8e597888a04d108934330fe3ea3965
lijiayan921 <2805327619@qq.com>
2023-02-13 17:14:50 +0800

    test: 💍 测试cz

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,7 @@ const App: React.FC = () => {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">on the Vite and React logos to learn more</p>
     </div>
   );
 };
